b5c1739cd55267f61269c071df1f76ad9695f887
nemo <iiey@users.noreply.github.com>
2017-02-10 18:08:23 +0100

    Not start double if one already run

--- conky/startConky.sh
+++ conky/startConky.sh
@@ -3,6 +3,7 @@ process_num=$(ps -ef | grep conky | grep -v "grep" | wc -l)
 if [ "$process_num" -gt 1 ]; then
     echo "Conky's already running"
 else
+    killall conky
     {
     conky --config=$HOME/sources/dotfiles/conky/conkyrc_default &
     conky --config=$HOME/sources/dotfiles/conky/conkyrc_wordclock &
