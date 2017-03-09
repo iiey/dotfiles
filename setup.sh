ac3921e9dbe2ad10495d7614eccce6241434d660
nemo <iiey@users.noreply.github.com>
2017-03-09 19:17:37 +0100

    + Added sourcing myrc in bashrc + Changed script variable to lower case

--- setup.sh
+++ setup.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-
 #Declare variables
-CURRENT=$(pwd -P)
-TMUXCOLORS="$HOME/.config/tmux"
-DOTFILES=("ackrc" "apparix_bash" "ctags" "inputrc" "gitignore_global" "git_template" "gvimrc" "vimrc" "myrc" "tmux.conf")
+_current=$(pwd -P)
+_tmuxcolors="$HOME/.config/tmux"
+_dotfiles=("ackrc" "apparix_bash" "ctags" "inputrc" "gitignore_global" "git_template" "gvimrc" "vimrc" "myrc" "tmux.conf")
 case $OSTYPE in
   darwin*)
-    BASH_FILE=$HOME/.bash_profile
+    _bashfile=$HOME/.bash_profile
     ;;
   *)
-    BASH_FILE=$HOME/.bashrc
+    _bashfile=$HOME/.bashrc
     ;;
 esac
 
@@ -18,7 +17,7 @@ esac
 function show_usage() {
   echo -e "Usage: $0 [argument] \n"
   echo "Arguments:"
-  echo "--help (-h): display this help message"
+  echo "--help (-h): display this help"
   echo "--install (-i): choose which configuration should be used"
   echo -e "--update (-u): update dotfiles \n"
   exit 0;
@@ -26,25 +25,38 @@ function show_usage() {
 
 #Back up existing configuration and link new one to dotfiles
 function link_file() {
-    SOURCE=$1
-    TARGET=$2
-    [ -f "$HOME/$TARGET" ] && cp -avL "$HOME/$TARGET" $CURRENT/backup/$SOURCE.bak
-    ln -sfn "$CURRENT/$SOURCE" "$HOME/$TARGET" && echo "Created symlink: ~/$TARGET@ --> $CURRENT/$SOURCE"
+    source=$1
+    target=$2
 
-    [ "$1" == "gitignore_global" ] && mv $HOME/$2 $HOME/.gitignore
-    #TODO source .myrc in bash_file
+    #create backup (overwrite last one)
+    if [ -f "$HOME/$target" ]; then
+        [ ! -d "$_current/backup" ] && mkdir -p $_current/backup
+        cp -avL "$HOME/$target" $_current/backup/$source.bak
+        echo "Backed up $_current/backup/$source.bak"
+    fi
+
+    ln -sfn "$_current/$source" "$HOME/$target" && echo "Created symlink: ~/$target@ --> $_current/$source"
+
+    #rename gitignore
+    [ "$source" == "gitignore_global" ] && mv $HOME/$2 $HOME/.gitignore
+    #add extra configuration to bashrc
+    if [ "$source" == "myrc" ] && ! $(grep -q "source.*$target" $_bashfile); then
+        echo "[ -f ~/$target  ] && source ~/$target" >> $_bashfile
+    fi
 }
 
 #copy tmux themes
 function sync_themes() {
-    if [ -n "$(type -t tmux)" ] && [ -d "$TMUXCOLORS" ]; then
-        echo -e "Copying themes to $TMUXCOLORS...\n"
-        rsync -aiu $CURRENT/tmuxcolors/ $TMUXCOLORS
+    if [ -n "$(type -t tmux)" ] && [ -d "$_tmuxcolors" ]; then
+        echo -e "Copying themes to $_tmuxcolors...\n"
+        rsync -aiu $_current/tmuxcolors/ $_tmuxcolors
     fi
 }
 
+
+#TODO setup.sh [package] just install specific stuff we need
 #Parse parameter
-#FIXME emtpy parameter not work
+[ $# -eq 0 ] && show_usage; exit 0
 for param in "$@"; do
   shift
   case "$param" in
@@ -70,7 +82,7 @@ shift $(expr $OPTIND - 1)
 #Install
 if [ $install ]; then
     echo "*** Installing dotfiles..."
-    for dotfile in ${DOTFILES[@]}; do
+    for dotfile in ${_dotfiles[@]}; do
         read -e -n 1 -p "Would you like to setup $dotfile? [y/N/q]: " resp
         case $resp in
             [yY])
@@ -114,3 +126,5 @@ if [ $update ]; then
 fi
 
 #TODO uninstall
+#remove source file
+#pat="source.*myrc"; if grep "$pat" $_bashfile; then sed -i "/$pat/d" $_bashfile
