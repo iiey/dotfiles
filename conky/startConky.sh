e4e39c3d641c3fc65471d24ad9eb7a336a6bf983
nemo <iiey@users.noreply.github.com>
2017-03-23 18:08:26 +0100

    Minor changes

--- conky/startConky.sh
+++ conky/startConky.sh
@@ -3,8 +3,8 @@ process_num=$(ps -ef | grep conky | grep -v "grep" | wc -l)
 if [ "$process_num" -gt 1 ]; then
     echo "Conky's already running"
 else
-    killall conky
     {
+    killall conky
     conky --config=$HOME/sources/dotfiles/conky/conkyrc_default &
     conky --config=$HOME/sources/dotfiles/conky/conkyrc_wordclock &
     } &>/dev/null
