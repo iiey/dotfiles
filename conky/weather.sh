cecb30d8498d2a275bfad1703bf062bea44d138d
nemo <iiey@users.noreply.github.com>
2019-07-03 21:00:00 +0200

    Do shellcheck for scripts

--- conky/weather.sh
+++ conky/weather.sh
@@ -2,11 +2,12 @@
 #AccuWeather (r) RSS weather for conky
 METRIC=1 #0 for F, 1 for C
 
-if [ -z ${1} ]; then
+if [ -z "$1" ]; then
     set -- "EUR|DE|GM017|ILMENAU"
-elif [ ${1} = "-h" ]; then
+elif [ "$1" = "-h" ]; then
     echo "USAGE: $0 <locationcode>"
     echo "Example: EUR|DE|GM017|BERLIN"
 fi
 
-curl -s http://rss.accuweather.com/rss/liveweather_rss.asp\?metric\=${METRIC}\&locCode\=$1 | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
+URL="http://rss.accuweather.com/rss/liveweather_rss.asp?metric=${METRIC}&locCode=$1"
+curl -s "$URL" | perl -ne 'if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; print "$1"; }'
--- setup.sh
+++ setup.sh
@@ -30,18 +30,18 @@ function link_file() {
 
     #create backup (overwrite last one)
     if [ -f "$HOME/$target" ]; then
-        [ ! -d "$_current/backup" ] && mkdir -p $_current/backup
-        cp -avL "$HOME/$target" $_current/backup/$source.bak
+        [ ! -d "$_current/backup" ] && mkdir -p "${_current}/backup"
+        cp -avL "$HOME/$target" "${_current}/backup/${source}.bak"
         echo "Backed up $_current/backup/$source.bak"
     fi
 
     ln -sfn "$_current/$source" "$HOME/$target" && echo "Created symlink: ~/$target@ --> $_current/$source"
 
     #rename gitignore
-    [ "$source" == "gitignore_global" ] && mv $HOME/$2 $HOME/.gitignore
+    [ "$source" == "gitignore_global" ] && mv "${HOME}/$2" "${HOME}"/.gitignore
     #add extra configuration to bashrc
-    if [ "$source" == "myrc" ] && ! $(grep -q "source.*$target" $_bashfile); then
-        echo "[ -f ~/$target  ] && source ~/$target" >> $_bashfile
+    if [ "$source" == "myrc" ] && ! grep -q "source.*$target" "$_bashfile"; then
+        echo "[ -f ~/$target  ] && source ~/$target" >> "$_bashfile"
     fi
 }
 
@@ -49,7 +49,7 @@ function link_file() {
 function sync_themes() {
     if [ -n "$(type -t tmux)" ] && [ -d "$_tmuxcolors" ]; then
         echo -e "Copying themes to $_tmuxcolors...\n"
-        rsync -aiu $_current/tmuxcolors/ $_tmuxcolors
+        rsync -aiu "${_current}/tmuxcolors/" "$_tmuxcolors"
     fi
 }
 
@@ -76,17 +76,18 @@ while getopts "hiu" opt; do
   ?) show_usage; exit 1 ;;
   esac
 done
-shift $(expr $OPTIND - 1)
+#shift $(expr $OPTIND - 1)
+shift "$((OPTIND - 1))"
 
 
 #Install
 if [ $install ]; then
     echo "*** Installing dotfiles..."
-    for dotfile in ${_dotfiles[@]}; do
-        read -e -n 1 -p "Would you like to setup $dotfile? [y/N/q]: " resp
+    for dotfile in "${_dotfiles[@]}"; do
+        read -r -n 1 -p "Would you like to setup $dotfile? [y/N/q]: " resp; echo
         case $resp in
             [yY])
-                link_file $dotfile .$dotfile;;
+                link_file "$dotfile" ".${dotfile}";;
             [nN]|"")
                 continue;;
             [qQ])
@@ -107,7 +108,7 @@ fi
 
 #Update
 if [ $update ]; then
-    read -e -n 1 -p "Update dotfiles? [Y/n]: " resp
+    read -r -n 1 -p "Update dotfiles? [Y/n]: " resp; echo
     case $resp in
         [yY]|"")
             git pull;;
@@ -116,7 +117,7 @@ if [ $update ]; then
     esac
     unset resp
 
-    read -e -n 1 -p "Update colorschemes? [y/N]: " resp
+    read -r -n 1 -p "Update colorschemes? [y/N]: " resp; echo
     case $resp in
         [yY])
             sync_themes;;
