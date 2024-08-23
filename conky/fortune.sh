e24b86cc43e1448c856942dec1865b57c21f9d0e
nemo <iiey@users.noreply.github.com>
2024-08-23 10:30:23 +0200

    conky: Update fortune local path

--- conky/fortune.sh
+++ conky/fortune.sh
@@ -2,7 +2,7 @@
 if type fortune > /dev/null 2>&1; then
     #display short fortune if personal dictionary not exists
     #place your dictionaries here or change dir path
-    FILE=$HOME/sources/dict/fortunes
+    FILE=$HOME/src/fortunes
     [ -r "$FILE" ] && ARG="$FILE" || ARG="-s"
     #use tux figure if cowsay exists
     fortune "$ARG" | ( command -v cowsay > /dev/null 2>&1 && cowsay -f tux || cat )
