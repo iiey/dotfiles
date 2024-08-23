10a60c9950e202a2309e4c65e8b10eb833f0dd11
nemo <iiey@users.noreply.github.com>
2024-08-23 19:27:49 +0200

    i3: Update script for setting wallpapers

--- config/i3/autostart/display.sh
+++ config/i3/autostart/display.sh
@@ -3,13 +3,14 @@
 #first file will set on screen 0, second on screen1, and so on
 
 #use only for i3wm session
-[ "$DESKTOP_SESSION" != "i3" ] && return
+[ "$DESKTOP_SESSION" != "i3" ] && exit
 
 #1. arrange personal dual monitors appropriately
-xrandr --output DP-0 --primary --output DVI-D-0 --left-of DP-0
+#num_monitors=$(xrandr --query | grep ' connected' | wc -l)
+#xrandr --output DP-0 --primary --output DVI-D-0 --left-of DP-0
 
 #2. setup background wallpapers
 if  command -v feh >/dev/null 2>&1; then
-    feh --bg-fill ~/Pictures/wallpaper/nebula.jpg \
-                  ~/Pictures/wallpaper/planet.jpg
+    feh --bg-fill ~/Pictures/wallpapers/solarsystem.jpg \
+                  ~/Pictures/wallpapers/planet.jpg
 fi
